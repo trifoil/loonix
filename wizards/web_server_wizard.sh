07275bf5547c3a7da2ea91831bfad070bc0ce453
trifoil <unixsslayer@gmail.com>
2024-05-08 21:27:09 +0200

    httpd runnning

--- wizards/web_server_wizard.sh
+++ wizards/web_server_wizard.sh
@@ -3,11 +3,25 @@
 display_menu() {
     echo "| What do you want to do?      |"
     echo "| 0. Install web server        |"
+    echo "| 1. Show httpd status         |"
     echo "| q. Quit                      |"
 }
 
 install_web_server() {
-    echo "test"
+    echo "Installing required packages"
+    dnf -y install httpd mod_ssl mod_md 
+    echo "Done"
+    echo "Adding the service to firewall"
+    firewall-cmd --add-service=https --permanent
+    firewall-cmd --add-service=http  --permanent
+    firewall-cmd --reload
+    echo "Starting and enabling the service"
+    systemctl start httpd
+    systemctl enable httpd
+}
+
+show_httpd_status(){
+    systemctl status httpd
 }
 
 main() {
@@ -20,8 +34,8 @@ main() {
         display_menu
         read -p "Enter your choice: " choice
         case $choice in
-            0) display_unix_users ;;
-            1) install_web_server ;;
+            0) install_web_server ;;
+            1) show_httpd_status ;;
             q|Q) clear && echo "Exiting the web server configuration wizard." && exit ;;
             *) clear && echo "Invalid choice. Please enter a valid option." ;;
         esac
