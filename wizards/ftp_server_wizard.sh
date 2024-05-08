9380c8f9f469218651cb12725dcae767276d3d91
trifoil <unixsslayer@gmail.com>
2024-05-08 21:42:11 +0200

    ftp debut

---
+++ wizards/ftp_server_wizard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+display_menu() {
+    echo "| What do you want to do?       |"
+    echo "| 0. Install ftp server (vsftpd)|"
+    echo "| 1.           |"
+    echo "| q. Quit                       |"
+}
+
