e1d40ec738b474b67a8a75e50e0b9669f68a578f
trifoil <unixsslayer@gmail.com>
2024-05-19 23:24:20 +0200

    smb menu

--- wizards/a_smb_share_wizard.sh
+++ wizards/a_smb_share_wizard.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
 
-archive_function() {
-# get the directory path input from the user
-echo "Please enter the directory path :"
-read directory_path
-
-# Check if the directory path is empty
-if [ -z "$directory_path" ]; then
-    echo "Error: Directory path cannot be empty."
-    exit 1
-fi
-
-# Check if the directory already exists
-if [ -d "$directory_path" ]; then
-    echo "Directory '$directory_path' already exists."
-    exit 0
-fi
-
-#create the dir
-mkdir -p "$directory_path"
-
-#check if the dir is created correctly
-if [ $? -eq 0 ]; then
-    echo "Directory '$directory_path' created successfully."
-else
-    echo "Failed to create directory '$directory_path'."
-    exit 1
-fi
-}
+RED='\033[0;31m' #Red
+GREEN='\033[0;32m' #Red
+NC='\033[0m' # No Color
 
 display_menu() {
-    echo "| What do you want to do?      |"
-    echo "| 0. Display UNIX users        |"
-    echo "| 1. Display SMB user          |"
-    echo "| 2. Add UNIX users            |"
-    echo "| 3. Add SMB user              |"
-    echo "| 4. Remove SMB user           |"
-    echo "| 5. Remove all SMB users      |"
-    echo "| 6. Disable SMB user          |"
-    echo "| 7. Enable SMB user           |"
-    echo "| q. Quit                      |"
-
+    echo "|-------------------------------------------|"
+    echo "|            ${GREEN}SMB server wizard${NC}              |"
+    echo "|-------------------------------------------|"
+    echo "|         What do you want to do?           |"
+    echo "|-------------------------------------------|"
+    echo "| 0. Display UNIX users                     |"
+    echo "| 1. Display SMB user                       |"
+    echo "| 2. Add UNIX users                         |"
+    echo "| 3. Add SMB user                           |"
+    echo "| 4. Remove SMB user                        |"
+    echo "| 5. Remove all SMB users                   |"
+    echo "| 6. Disable SMB user                       |"
+    echo "| 7. Enable SMB user                        |"
+    echo "|-------------------------------------------|"
+    echo "| q. Quit                                   |"
+    echo "|-------------------------------------------|"
+    echo ""
 }
 
 display_unix_users() {
