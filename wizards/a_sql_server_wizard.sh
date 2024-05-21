756e20c2c74228eacc59c3dacf85d44d48ddbd3a
mankouuu <mankoulani.fofanah@std.heh.be>
2024-05-21 12:10:18 +0200

    lol

--- wizards/a_sql_server_wizard.sh
+++ wizards/a_sql_server_wizard.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+dnf install phpMyAdmin
+dnf install php
+
+service httpd restart
+
+
+
+edit_profil(){}
+
+delete_profil(){}
+
+
+
+    while true; do
+        display_menu
+        read -p "Enter your choice: " choice
+        case $choice in
+            0) edit_profil ;;
+            1) delete_profil ;;
+            
+            q|Q) clear && echo "Exiting the web server configuration wizard." && exit ;;
+            *) clear && echo "Invalid choice. Please enter a valid option." ;;
+        esac
+    done
+}
+
+main
