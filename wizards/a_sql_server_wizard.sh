b9a387ad7c0319234995e207175dca6b1d2dd261
Dorian <37149267+kunras@users.noreply.github.com>
2024-05-23 02:04:20 +0200

    Setup de base pour mysql

--- wizards/a_sql_server_wizard.sh
+++ wizards/a_sql_server_wizard.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-dnf install phpMyAdmin
-dnf install php
-
+dnf install phpMyAdmin mariadb-server mariadb -y
+sed -i '\|<Directory /usr/share/phpMyAdmin>|,\|</Directory>| s|Require local|Require all granted|' /etc/httpd/conf.d/phpMyAdmin.conf
+sudo ln -s /usr/share/phpMyAdmin /srv/mankou/phpMyAdmin
+systemctl enable --now mariadb
 service httpd restart
 
 
+# mysql_secure_installation mais en bash + ajout d'un utilisateur admin
+# disallow remote login for root
+sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+# Kill the anonymous users
+sudo mysql -e "DROP USER ''@'localhost';"
+# Because our hostname varies we'll use some Bash magic here.
+sudo mysql -e "DROP USER ''@'$(hostname)';"
+# Kill off the demo database
+sudo mysql -e "DROP DATABASE test;"
+# Create a admin user
+sudo mysql -e "CREATE USER 'mankou'@'localhost' IDENTIFIED BY 'test123';"
+# Grant all privileges on all databases to mankou (admin user)
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'mankou'@'localhost' WITH GRANT OPTION;"
+# Make our changes take effect
+sudo mysql -e "FLUSH PRIVILEGES"
+
+display_menu() {
+    echo "| What do you want to do?         |"
+    echo "| 0. Edit profil                  |"
+    echo "| 1. delete profil                |"
+    echo "| q. Quit                         |"
+}
 
 edit_profil(){}
 
