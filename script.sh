a6748a22953697da8f5f00f8190522aef72e657a
trifoil <unixsslayer@gmail.com>
2024-05-23 10:31:34 +0200

    a

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 clear
 
 RED='\033[0;31m' #Red
@@ -7,6 +9,9 @@ GREEN='\033[0;32m' #Red
 NC='\033[0m' # No Color
 
 updatedb
+systemctl enable --now cockpit.socket
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
 
 # Function to display the menu
 display_menu() {
